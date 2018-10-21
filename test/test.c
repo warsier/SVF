244ffa07bd4b179d1730f7428a2c54898c67dfb1
warsier <787012293@qq.com>
2018-10-21 01:07:46 -0400

    temp

---
+++ test/test.c
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int f1(z, y)
+{
+	return (z * y);
+}
+
+int f2(y)
+{
+	return (y + 2);
+}
+
+int main()
+{
+	int n;
+	cin << n;
+	int z = 0;
+	int y = 0;
+	int i = 1;
+	while (i < n) {
+		z = f1(z, y);
+		y = f2(y);
+		i++;
+	}
+	cout << z;
+	return 0;
+}
